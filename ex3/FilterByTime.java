fe37e9d19e1cf9d74a5f7a3c15623b61c112f204
lz9953 <472795443@qq.com>
2016-11-03 15:25:06 +0800

    Update FilterByTime.java

--- ex3/FilterByTime.java
+++ ex3/FilterByTime.java
@@ -19,19 +19,34 @@ public class FilterByTime {
 	
 	public static void main(String[] args) throws ParseException, FileNotFoundException{
 		SimpleDateFormat regularFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Locale  locale = Locale.US;
+		SimpleDateFormat regularFormat1 = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss",locale);//定义时间格式，用于转换从文件中提取得时间
 		Date beginDate = regularFormat.parse("2015-12-31 18:00:00");
 		Date endDate = regularFormat.parse("2015-12-31 19:00:00");
-		String filePath = "./access.log";
-		FileInputStream inputStream = new FileInputStream(filePath);
-		Scanner scanner = new Scanner(inputStream, "UTF-8");
+		String filePath = "./access.log";//指定文件路径
+		FileInputStream inputStream = new FileInputStream(filePath);//读取本地文件中的字节数据
+		Scanner scanner = new Scanner(inputStream, "UTF-8");//
 		while (scanner.hasNext()){
 			// 对每行进行处理
 			String line = scanner.nextLine();
 			// 切分获取IP，Time
 			String strIp = null;
 			String strTime = null;
-			// 对在时间区间内的数据进行输出
-			System.out.println(strIp + "\t" + strTime);
+			Pattern pattern =  Pattern.compile("(\\d+.\\d+.\\d+.\\d+).*(\\[.*\\]).*");
+    			Matcher matcher = pattern.matcher(line);
+    		if(matcher.find()){
+    			strIp = matcher.group(1);
+        		
+        		String[] t = matcher.group(2).split(" ");
+    			strTime = t[0].substring(1, t[0].length());//对正则表达式中的第二项 去除"["
+    			Date StrTime = regularFormat1.parse(strTime);//将提取得时间转换为date格式，用于区间比较
+    			// 对在时间区间内的数据进行输出
+    			if(StrTime.after(beginDate) && StrTime.before(endDate)){
+    				System.out.println(strIp + "\t" + StrTime);
+    			}
+    		}
+    		
+			
 		}
 	}
 	
